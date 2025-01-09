901875712e8bbd68be53cb0e3d29126025441440
Bruno Frasan <brunofrancco@gmail.com>
2025-01-09 09:27:49 +0000

    Completed lab

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,57 @@
 // src/types.ts
 
 export type Location = {
-    id: number;
-    name: string;
-    latitude: number;
-    longitude: number;
-    elevation: number;
-    feature_code: string;
-    country_code: string;
-    timezone: string;
-    population: number;
-    postcodes: string[];
-    country_id: number;
-    country: string;
-    admin1?: string;
-    admin2?: string;
-    admin3?: string;
-    admin4?: string;
-    admin1_id?: number;
-    admin2_id?: number;
-    admin3_id?: number;
-    admin4_id?: number;
+	id: number
+	name: string
+	latitude: number
+	longitude: number
+	elevation: number
+	feature_code: string
+	country_code: string
+	timezone: string
+	population: number
+	postcodes: string[]
+	country_id: number
+	country: string
+	admin1?: string
+	admin2?: string
+	admin3?: string
+	admin4?: string
+	admin1_id?: number
+	admin2_id?: number
+	admin3_id?: number
+	admin4_id?: number
 }
 
 export type LocationResponse = {
-    results?: Location[];
-    generationtime_ms: number;
+	results?: Location[]
+	generationtime_ms: number
 }
 
+export type WeatherResponse = {
+	latitude: number
+	longitude: number
+	generationtime_ms: number
+	utc_offset_seconds: number
+	timezone: string
+	timezone_abbreviation: string
+	elevation: number
+	current_weather_units: {
+		time: string // "iso8601"
+		interval: string // "seconds"
+		temperature: string // "°C"
+		windspeed: string // "km/h"
+		winddirection: string // "°"
+		is_day: string // Could be empty or string
+		weathercode: string // "wmo code"
+	}
+	current_weather: {
+		time: string // e.g., "2025-01-08T15:00"
+		interval: number // e.g., 900
+		temperature: number // e.g., 3.2
+		windspeed: number // e.g., 12.2
+		winddirection: number // e.g., 223
+		is_day: number // e.g., 1
+		weathercode: number // e.g., 1
+	}
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,91 @@
 // src/utils.ts
 
-import axios from 'axios';
-import { LocationResponse, Location } from "./types";
-
-
+import axios from 'axios'
+import { LocationResponse, Location, WeatherResponse } from './types'
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+	const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`
+	return axios.get(url).then((response) => {
+		return response.data
+	})
 }
 
+export async function getCurrentWeather(
+	locationDetails: Location
+): Promise<WeatherResponse> {
+	const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`
+	const response = await fetch(url)
+	const data = await response.json()
+	console.log(data)
 
+	return data
+	// return axios.get(url).then((response) => response.data)
+}
+// export function getCurrentWeather(
+// 	locationDetails: Location
+// ): Promise<WeatherResponse> {
+// 	const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`
+// 	return axios.get(url).then((response) => response.data)
+// }
 
+export function displayLocation(locationDetails: Location): void {
+	const loc = document.getElementById('location-name') as HTMLElement
+	const coun = document.getElementById('country') as HTMLElement
+	loc.innerText = locationDetails.name
+	coun.innerText = locationDetails.country
+}
+
+export function displayWeatherData(obj: WeatherResponse): void {
+	const tempe = document.getElementById('temperature') as HTMLElement
+	const wind = document.getElementById('windspeed') as HTMLElement
+	const windDir = document.getElementById('winddirection') as HTMLElement
+	tempe.innerText = `Temperature: ${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`
+	wind.innerText = `Wind Speed: ${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`
+	windDir.innerText = `Wind Direction: ${obj.current_weather.winddirection} ${obj.current_weather_units.winddirection}`
+}
+
+export function updateBackground(weatherCode: number, isDay: number): void {
+	const body = document.getElementsByTagName('body')[0] as HTMLElement
+
+	if (weatherCode === 0 || weatherCode === 1) {
+		if (isDay === 0) {
+			body.classList.add('sunny-night')
+		} else {
+			body.classList.add('sunny')
+		}
+	} else if (weatherCode === 2) {
+		if (isDay === 0) {
+			body.classList.add('partly-cloudy-night')
+		} else {
+			body.classList.add('partly-cloudy')
+		}
+	} else if (weatherCode === 3) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('cloudy')
+		}
+	} else if (weatherCode === 4) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('foggy')
+		}
+	} else if (weatherCode === 5) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('drizzle')
+		}
+	} else if (weatherCode === 6) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('rain')
+		}
+	} else if (weatherCode === 7) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('snow')
+		}
+	} else if (weatherCode === 8) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('showers')
+		}
+	} else if (weatherCode === 9) {
+		if (isDay === 0 || isDay === 1) {
+			body.classList.add('thunderstorm')
+		}
+	}
+}
